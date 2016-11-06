08bb771443f90426eff082fcce75910ccf085ec3
Danny Kirschner <rkirschner1377@gmail.com>
2016-11-06 13:11:54 -0500

    Using send_file to serve files in Sinatra. Changing the route to a POST for our writes to the json file. I had to place {} in the empty json file for it to write correctly. Using cURL to test the POST to favorites. Changed erroneous use of << to add to hash to merge! as we likely do not want multiples of the same favorites in our data.

--- app.rb
+++ app.rb
@@ -1,22 +1,28 @@
 require 'sinatra'
+require 'JSON'
 
 class App < Sinatra::Application 
   get '/' do
-    File.read('views/index.html')
-  end
-
-  get 'favorites' do
-    response.header['Content-Type'] = 'application/json'
-    File.read('data.json')
+    send_file ('views/index.html')
   end
 
   get '/favorites' do
+    response.header['Content-Type'] = 'application/json'
+    send_file 'data.json' #File.join(settings.public_folder, 'data.json')
+  end
+
+  # to test backend:
+  # curl -X POST -H "Content-Type: application/json" -d '{"name":"Star Wars", "oid": "1"}' http://localhost:9292
+  post '/favorites' do
+    logger.info "post received...\n params: #{params}"
+    logger.info "request body: \n #{request.body.read}" - reading request body from curl
     file = JSON.parse(File.read('data.json'))
-    unless params[:name] && params[:oid]
-      return 'Invalid Request'
-    end
+    #unless params[:name] && params[:oid]
+    #  return 'Invalid Request'
+    #end
     movie = { name: params[:name], oid: params[:oid] }
-    file << movie
+    file.merge!(movie)
+    logger.info file.to_json
     File.write('data.json',JSON.pretty_generate(file))
     movie.to_json
   end
