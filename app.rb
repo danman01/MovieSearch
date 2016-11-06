ab895c1c50d05bbc214b59bfd76bbe13c72812ec
Danny Kirschner <rkirschner1377@gmail.com>
2016-11-06 11:47:45 -0500

    Fixing errors in app.rb: syntax errors and adding views directory to the / route to find index.html. Also wrapping the app in a class that inherits from Sinatra::Application to give logging capability. Adding config.ru file to prepare for Heroku deployment.

--- app.rb
+++ app.rb
@@ -1,20 +1,23 @@
 require 'sinatra'
 
-get '/'
-  File.read('index.html')
-end
+class App < Sinatra::Application 
+  get '/' do
+    File.read('views/index.html')
+  end
 
-get 'favorites' do
-  response.header['Content-Type'] = 'application/json'
-  File.read('data.json')
-end
+  get 'favorites' do
+    response.header['Content-Type'] = 'application/json'
+    File.read('data.json')
+  end
 
-get '/favorites' do
-  file = JSON.parse(File.read('data.json'))
-  unless params[:name] && params[:oid]
-    return 'Invalid Request'
-  movie = { name: params[:name], oid: params[:oid] }
-  file << movie
-  File.write('data.json',JSON.pretty_generate(file))
-  movie.to_json
+  get '/favorites' do
+    file = JSON.parse(File.read('data.json'))
+    unless params[:name] && params[:oid]
+      return 'Invalid Request'
+    end
+    movie = { name: params[:name], oid: params[:oid] }
+    file << movie
+    File.write('data.json',JSON.pretty_generate(file))
+    movie.to_json
+  end
 end
