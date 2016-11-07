7ac6e45a95a1f31bc2534871556e98dc64701dd8
Danny Kirschner <rkirschner1377@gmail.com>
2016-11-06 22:09:57 -0500

    Adding loading gif, wrapping functions in application.js in a function so they have scope

--- public/application.js
+++ public/application.js
@@ -1,14 +1,19 @@
-function activate_movie_search(){
+(function() {
+var loading = document.getElementById("loading");
+var noResults = document.getElementById("noResults");
+var ul = document.getElementById("movieList");
 
+
+function activate_movie_search(){
   var searchForm = document.getElementById("movieSearch");
-  console.log("got here");
   searchForm.onsubmit = function(form){
+
+    reset_page();
     var title = form.srcElement.elements.namedItem("s").value;
     console.log("form submitted with " + title);
-    var response = search_api(title);
-    update_page(response);
+    search_api(title);
   }
-
+  document.getElementById("searchButton").disabled=false;
 }
 
 function search_api(title){
@@ -26,29 +31,45 @@ function search_api(title){
       if (xhr.status === OK) 
         response = xhr.responseText
           console.log('Success: ' + response); // 'This is the returned text.'
+        update_page(JSON.parse(response))
     } else {
       response = xhr.status;
       console.log('Error: ' + response); // An error occurred during the request.
     }
   }
-
-  return response;
 };
 
 function update_page(response){
-
-  // update dom
-  var title = response['title'];
-  var html = _build_movie_html(title);
-
-  var ul = document.getElementById("movieList");
-  ul.appendChild(html);
+  if(response.Search != undefined) {
+    for(i=0; i < response.Search.length; i++){ 
+      var html = _build_movie_html(response.Search[i]);
+      window.ul.append(html);
+    }
+  }
+  else {
+    window.noResults.style.display="block";
+  }
+  window.loading.style.display = "none";
 }
 
-function _build_movie_html(title) {
-  // build elements for page
-  var li = document.createElement("<li>");
-  return li;
+function _build_movie_html(result) {
+  var el = document.createElement("li");
+  for(prop in result){
+    if(prop == "Title"){
+      var detail = document.createElement('h3');
+      detail.innerHTML = "<a href='#' data-onclick='showDetails'>" + result[prop] + "</a>";
+    }
+    else if(prop == "Poster" && result[prop] != "N/A"){
+      var detail = document.createElement('img');
+      detail.src = result[prop];
+    }
+    else {
+      var detail = document.createElement('p');
+      detail.innerHTML = prop + ": " + result[prop];
+    }
+    el.append( detail ); 
+  }
+  return el;
 }
 
 function show_movie_details(oid){
@@ -63,11 +84,16 @@ function display_favorites(){
   // show all favorites from data.json
 }
 
+function reset_page(){
+  window.loading.style.display = "block";
+  window.noResults.style.display="none";
+  //window.ul.innerHTML="";
+
+}
 
 // Make sure the DOM is loaded before setting up our functions
 document.addEventListener('DOMContentLoaded', function(){ 
-  console.log("loaded");
   activate_movie_search();
 })
 
-
+})();
